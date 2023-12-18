7ca7946a73862f40786b80e1562e38e4706aa398
Konrad <konrad.babowicz21@gmail.com>
2023-12-18 22:08:03 +0000

    showing the content on html page

--- src/modules/home.js
+++ src/modules/home.js
@@ -3,5 +3,13 @@ export default function renderHome() {
     const h2Text = document.createElement('h2');
     const h1Text = document.createElement('h1');
     const pText = document.createElement('p');
+    const infoDiv = document.createElement('div');
+    const infoButton = document.createElement('button');
+
+    homeContent.appendChild(h2Text);
+    homeContent.appendChild(h1Text);
+    homeContent.appendChild(pText);
+    homeContent.appendChild(infoDiv);
+    infoDiv.appendChild(infoButton);
 
 }
