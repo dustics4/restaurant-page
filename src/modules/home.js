dc607f29883e6df6413c933d5d2864af3eea82ae
Konrad <konrad.babowicz21@gmail.com>
2023-12-18 22:04:57 +0000

    creating elements in home page

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,3 +1,7 @@
 export default function renderHome() {
     const homeContent = document.createElement('div');
+    const h2Text = document.createElement('h2');
+    const h1Text = document.createElement('h1');
+    const pText = document.createElement('p');
+
 }
