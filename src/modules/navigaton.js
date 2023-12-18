80475437ec8cb5aebe57fb393a34cd2d4b877b1e
Konrad <konrad.babowicz21@gmail.com>
2023-12-18 21:35:08 +0000

    creating buttons using js file now

---
+++ src/modules/navigaton.js
@@ -0,0 +1,8 @@
+const navBar = () => {
+    const nav = document.createElement('nav');
+    const homeButton = document.createElement('button');
+    const aboutButton = document.createElement('button');
+    const contactButton = document.createElement('button');
+
+    
+}
